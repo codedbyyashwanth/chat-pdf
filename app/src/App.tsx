bf93b2d8055e48923c1686a72c237086a992d929
codedbyyashwanth <yashwanthpcmc5@gmail.com>
2025-02-12 00:36:46 +0100

    feat: Add theme provider and mode toggle component; configure Tailwind CSS and utility functions

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "./components/mode-toggle"
 
 const App = () => {
     return <div>
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
+        <Button>Submit Me</Button>
+        <ModeToggle />
     </div>
 }
 
