e7f70a053ba639c5bf98972f341349f789a19c34
codedbyyashwanth <yashwanthpcmc5@gmail.com>
2025-02-20 03:02:45 +0100

    feat: Add HeroContainer component for document upload functionality; update Navbar layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button"
 import Navbar from "./components/navbar"
+import { HeroContainer } from "./components/heroContainer"
 
 
 const App = () => {
     return <div className="min-h-[200vh]">
-        <Navbar />        
+        <Navbar />
+        <HeroContainer />        
     </div>
 }
 
