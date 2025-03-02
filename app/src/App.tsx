b995727c0721b4acc0575f089719bb0478ced14d
codedbyyashwanth <yashwanthpcmc5@gmail.com>
2025-03-02 01:16:50 +0100

    feat: Replace UploadPdf component with HeroContainer in App layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,12 @@
 import Navbar from "./components/navbar"
 import { HeroContainer } from "./components/heroContainer"
-import { UploadPdf } from "./components/fileUpload"
 
 
 
 const App = () => {
     return <div className="min-h-[90vh]">
         <Navbar />
-        {/* <HeroContainer /> */}
-        <UploadPdf />
+        <HeroContainer />
     </div>
 }
 
